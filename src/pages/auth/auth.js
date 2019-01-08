70699640b1cb430055f1294771dd15e421695e2a
motasimfoad <motasimfoad@gmail.com>
2019-01-08 21:21:44 +0600

    Linking and routing

---
+++ src/pages/auth/auth.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../auth/auth.css';
+
+class Auth extends Component {
+  render() {
+    return (
+      <div className="auth_demo">
+        Auth Page
+      </div>
+    );
+  }
+}
+
+export default Auth;
---
+++ src/pages/main/main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../main/main.css';
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="main_demo">
+        main page
+      </div>
+    );
+  }
+}
+
+export default Main;
