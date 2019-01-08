5239e98925fadd8fb584fbe92ee76680fc000c56
motasimfoad <motasimfoad@gmail.com>
2019-01-08 21:26:11 +0600

    Adding helmet

--- src/pages/auth/auth.js
+++ src/pages/auth/auth.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import {Helmet} from "react-helmet";
 import '../auth/auth.css';
 
 class Auth extends Component {
   render() {
     return (
       <div className="auth_demo">
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>Auth Page</title>
+        </Helmet>
         Auth Page
       </div>
     );
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import {Helmet} from "react-helmet";
 import '../main/main.css';
 
 class Main extends Component {
   render() {
     return (
       <div className="main_demo">
-        main page
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>Main Page</title>
+        </Helmet>
+        Main Page
       </div>
     );
   }
