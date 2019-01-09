55c43c9b8e2efcd84adc556c22928373d26b7588
motasimfoad <motasimfoad@gmail.com>
2019-01-09 15:45:11 +0600

    Full screen background image

--- src/pages/demo_main/demo_main.js
+++ src/pages/demo_main/demo_main.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import {Helmet} from "react-helmet";
-import '../dashboard/dashboard.css';
+import '../demo_main/demo_main.css';
 import { Button } from 'react-mdl';
 
 class demo_main extends Component {
   render() {
     return (
-      <div className="dash_demo">
+      <div className="dm_demo">
         <Helmet>
             <meta charSet="utf-8" />
             <title>Dashboard Page</title>
