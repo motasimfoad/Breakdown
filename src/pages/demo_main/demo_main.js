c6320ee72ac9d0272935eebf323fbc027febdea7
motasimfoad <motasimfoad@gmail.com>
2019-01-09 14:44:44 +0600

    Added demo practice file named demo_main

---
+++ src/pages/demo_main/demo_main.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {Helmet} from "react-helmet";
+import '../dashboard/dashboard.css';
+import { Button } from 'react-mdl';
+
+class demo_main extends Component {
+  render() {
+    return (
+      <div className="dash_demo">
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>Dashboard Page</title>
+        </Helmet>
+        dm Page <br />
+        <Button raised colored>MDL</Button>
+      </div>
+    );
+  }
+}
+
+export default demo_main;
