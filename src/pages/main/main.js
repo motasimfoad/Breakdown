3c79b4109b47b146932e22c44d4b024bbf3dda74
milimdali56 <milimdali@gmail.com>
2019-01-17 03:18:22 +0600

    footer

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -4,10 +4,13 @@ import '../main/main.css';
 import {Col } from 'react-bootstrap';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fab, faFacebookF, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons'
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 import pic from './logo.png';
+import img from './flogo.png';
 
-library.add(faAngleDown);
+
+library.add(faAngleDown,fab );
 
 class Main extends Component {
   render() {
@@ -26,23 +29,54 @@ class Main extends Component {
             </div>
 
             <div className="icon">
-              <a href="#about"><FontAwesomeIcon icon={faAngleDown}/></a>
+              <FontAwesomeIcon icon={faAngleDown}/>
+              <FontAwesomeIcon icon={"faCheckSquare"} />
+
+              {/* <a href="#about"><FontAwesomeIcon icon={faAngleDown}/></a> */}
             </div>
 
           </div>
 
           <div className="small">
-            <a name="about"> 
+            {/* <a name="about">  */}
             <Col xs={2} md={3}></Col>
             <div className="col-xs-8 col-md-6">
             <h2>About us</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. Quisque nec mauris sit amet elit</p>
             </div>
             <Col xs={2} md={3}></Col>
-            </a>
+            {/* </a> */}
           </div>
 
-<footer> jvhjb</footer>
+    <footer >
+      <div className="col-xs-4 col-sm-4 col-md-4 f">
+      &copy; 2019 All rights reserved.
+      </div>
+      <div className="col-xs-4 col-sm-4 col-md-4 fl">
+      <img src={img} className="pic" alt="" />       
+      </div>
+      <div className="col-xs-4 col-sm-4 col-md-4 socialicon">
+      <ul>
+      <li>
+            <a href="https://www.instagram.com/motasimfoad/" className="insta">
+            <FontAwesomeIcon icon={faTwitter} />
+              </a>      
+            </li>
+      <li>
+            <a href="https://www.instagram.com/motasimfoad/" className="insta">
+            <FontAwesomeIcon icon={faInstagram} />
+              </a>      
+            </li>
+
+              <li>
+              <a href="https://www.instagram.com/motasimfoad/" className="fb">
+              <FontAwesomeIcon icon={faFacebookF} />
+              </a>      
+            </li>
+           
+      </ul>
+      </div>
+    </footer>
       </div>
     );
   }
